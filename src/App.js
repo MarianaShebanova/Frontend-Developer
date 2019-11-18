65ed0dea28d0955c4df46d6bd0f865fae4f35c59
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-11-18 13:11:57 -0700

    working on create function, form complete, post request error because i have no api to request from, validation in place

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import ProfilePage from './components/ProfilePage.js';
 import PrivateRoute from './components/PrivateRoute';
+import AddForm from "./components/AddForm";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <PrivateRoute path="/profile-page">
             <Route exact path="/profile-page" component={ProfilePage} />
         </PrivateRoute>
+        <AddForm />
       </div>
     </Router>
   );
