62a57c6bd3eeaa368b0a67103ffb81c36b86d9b4
ngaivoronski <ngaivoronski@gmail.com>
2019-11-19 09:37:48 -0500

    profile page with dummy data completed

--- TempServer.js
+++ TempServer.js
@@ -9,6 +9,32 @@ const token =
 app.use(bodyParser.json());
 app.use(CORS());
 
+let profile = {
+    id: 1,
+    username: "TestUser1",
+    password: "password",
+    email: "test1@gmail.com",
+    photo: "https://i.imgur.com/b0PRcsj.jpg",
+    age: 22,
+    location: "New York, USA",
+    posts: [
+        {
+            id: Date.now(),
+            date: "January 17, 2019",
+            title: "Mona Lisa",
+            img: "https://i.imgur.com/emqPiNO.png",
+            description: "Portrait of a woman by Leonardo da Vinci"
+        },
+        {
+            id: Date.now(),
+            date: "Februrary 22, 2019",
+            title: "The Starry Night",
+            img: "https://i.imgur.com/0kYlQt7.png",
+            description: "Landscape by Vincent van Gogh"
+        },
+    ],
+};
+
 function authenticator(req, res, next) {
     const { authorization } = req.headers;
     if (authorization === token) {
@@ -17,6 +43,10 @@ function authenticator(req, res, next) {
         res.status(403).json({ error: "User must be logged in to do that." });
     }
 }
+
+app.get("/profile", authenticator, (req, res) => {
+    res.send(profile);
+});
 
 app.post("/login", (req, res) => {
     const { username, password } = req.body;
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -4,6 +4,8 @@ import {axiosWithAuth} from "./axiosWithAuth";
 const ProfilePage = (props) => {
     const [profileData, setProfileData] = useState({
         username: "",
+        email: "",
+        photo: "",
         age: "",
         location: "",
         posts: [],
@@ -11,16 +13,24 @@ const ProfilePage = (props) => {
     // fetch the pofile data of the user when the component mounts
     // set the profile data to display the user's profile page
 
-    // useEffect(() => {
-    //     axiosWithAuth().get(`http://localhost:5000/profile`)
-    //     .then(response => {
-    //     console.log(response);
-    //     setColorList(response.data);
-    //     })
-    //     .catch(error => {
-    //     console.log(error);
-    //     })
-    // },[])
+    useEffect(() => {
+        axiosWithAuth().get(`http://localhost:5000/profile`)
+        .then(response => {
+        setProfileData({
+            username: response.data.username,
+            email: response.data.email,
+            photo: response.data.photo,
+            age: response.data.age,
+            location: response.data.location,
+            posts: response.data.posts,
+        })
+        })
+        .catch(error => {
+        console.log(error);
+        })
+    },[])
+
+    
 
     const LogOut = () => {
         sessionStorage.removeItem("token");
@@ -29,7 +39,20 @@ const ProfilePage = (props) => {
 
     return (
         <div className="profile-div">
-            <h1>Welcome to your profile!</h1>
+            <h1>{profileData.username}</h1>
+            <img src={profileData.photo}></img>
+            <p>Email: {profileData.email}</p>
+            <p>Age: {profileData.age}</p>
+            <p>location: {profileData.location}</p>
+            <p>Your posts:</p>
+            {profileData.posts.map(post => (
+                <div>
+                    <h2>{post.title}</h2>
+                    <img src={post.img} style={{'width': '200px', 'height':'auto'}}></img>
+                    <p>Posted on: {post.date}</p>
+                    <p>Description: {post.description}</p>
+                </div>
+            ))}
             <button onClick={LogOut}>Log Out</button>
         </div>
     );
